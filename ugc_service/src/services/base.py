3d692177a9cd956da769839a26f3eb7bc5f7bd3f
Евгений Зеленов <macbook@MacBook-Pro-Evgenij.local>
2023-01-09 02:08:39 +0600

    Black

--- ugc_service/src/services/base.py
+++ ugc_service/src/services/base.py
@@ -1,20 +1,21 @@
-import os
-import sys
+from fastapi import Depends
 
 from kafka_client.event_streamer import KafkaClient, get_kafka
 
 
-class BaseService:
+class EventService:
+
     def __init__(self, kafka: KafkaClient):
         self.kafka_client = kafka
 
-    def produce(self, key, **attrs):
-        model = self._model(**attrs)
+    def produce(self, key, topic_name, model) -> None:
         self.kafka_client.produce_message(
-            key=key,
-            topic=self._topic_name,
-            value=model.value,
+            key=str.encode(key),
+            topic=topic_name,
+            message=model.value.json(),
             timestamp_ms=model.timestamp_ms,
         )
 
-        return model.json()
+
+def get_event_service(kafka: KafkaClient = Depends(get_kafka)) -> EventService:
+    return EventService(kafka)
