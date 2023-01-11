fa6077a1c7c269b1b8fcbf46a3e102709218fdd8
nikita <nikitakhb@gmail.com>
2023-01-11 23:32:02 +1000

    fixed structure

--- ugc_service/src/db/clickhouse/migrator.py
+++ ugc_service/src/db/clickhouse/migrator.py
@@ -4,32 +4,55 @@ from clickhouse_driver import Client
 
 logging.basicConfig(level=logging.INFO)
 
-client = Client(host="clickhouse-node1")
+client = Client(host='clickhouse-node1')
+#client = Client(host='localhost')
 
 
-def ch_database(client: Client):
-    client.execute("CREATE DATABASE IF NOT EXISTS ugc ON CLUSTER company_cluster")
-    return True
+
+def ch_kafka_queue(client: Client):
+    client.execute(
+        """
+        CREATE TABLE entry_events_queue
+            (
+                timestamp DateTime,
+                event String
+            )
+        ENGINE = Kafka
+        SETTINGS
+        kafka_broker_list = 'kafka_dev:9192',
+        kafka_topic_list = 'entry-events',
+        kafka_group_name = 'group_events',
+        kafka_format = 'JSONEachRow'
+        """)
 
 
 def ch_table(client: Client):
     client.execute(
         """
-        CREATE TABLE ugc.views ON CLUSTER company_cluster 
+        CREATE TABLE entry_events
             (
-                user_id String,
-                filmwork_id String,
-                viewed_frame UInt64,
-                timestamp DateTime('Europe/Moscow') 
-            ) 
-        Engine=MergeTree() 
+                timestamp DateTime,
+                event String
+            )
+        ENGINE = MergeTree
         ORDER BY timestamp
+        """)
+
+
+def ch_kafa_consumer(client: Client):
+    client.execute(
         """
-    )
+        CREATE MATERIALIZED VIEW materialized_view TO entry_events
+        AS SELECT timestamp AS timestamp, event AS event
+        FROM entry_events_queue
+        ORDER BY timestamp
+        """)
 
 
-if __name__ == "__main__":
-    ch_database(client)
-    logging.info("created clickhouse database: ugc")
+if __name__ == '__main__':
+    ch_kafka_queue(client)
+    logging.info('created clickhouse kafka_dev queue table')
     ch_table(client)
-    logging.info("created clickhouse table: views")
+    logging.info('created clickhouse table: entry_events')
+    ch_kafa_consumer(client)
+    logging.info('created clickhouse kafka_dev consumer table')
