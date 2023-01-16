a2d6c855abe620c3932d9b8288965d78bff95a5d
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-01-17 01:41:00 +0600

    Update readme

--- research/main.py
+++ research/main.py
@@ -1,10 +1,9 @@
 from db.clickhouse.clickhouse_client import ch_client
 from db.clickhouse.data_scheme import init_ch
+from db.vertica.data_scheme import init_vertica
+from db.vertica.vertica_client import vt_client
 from speed_test import DBSpeedTest, VerticaSpeedTest
 from test_data.fake_data import data_generator, post_data
-from db.vertica.vertica_client import vt_client
-
-from db.vertica.data_scheme import init_vertica
 
 ch_speed_test = DBSpeedTest(ch_client)
 init_ch(ch_client)
@@ -18,7 +17,7 @@ def test_insert():
     b = vertica_speed_test.test_insert_data(data='test.csv',
                                             query="COPY test (id, viewpoint, date) FROM stdin DELIMITER ',' ")
     diff = b / a * 100 - 100
-    print(f"Clickhouse быстрее на {diff}%")
+    print(f"Clickhouse быстрее на {round(diff, 1)}%")
 
 
 test_insert()
