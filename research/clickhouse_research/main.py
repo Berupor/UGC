52504d9d2ea216b9a4bcdc80f27caad9d201020f
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-01-18 18:37:40 +0600

    Improve speedtest class

--- research/clickhouse_research/main.py
+++ research/clickhouse_research/main.py
@@ -1,10 +1,11 @@
+import os
+import sys
+
 from db.clickhouse.clickhouse_client import ch_client
 from db.clickhouse.data_scheme import flush_ch, init_ch
 from db.vertica.data_scheme import flush_vertica, init_vertica
 from db.vertica.vertica_client import vt_client
 from test_data.fake_data import data_generator
-import sys
-import os
 
 sys.path.append("..")
 from research.speed_test import CHSpeedTest, VerticaSpeedTest
@@ -19,20 +20,25 @@ init_vertica(vt_client.cursor())
 
 
 def test_insert():
-    a = ch_speed_test.test_insert_data(
+    ch_speed_test.test_insert_data(
         "INSERT INTO test VALUES", (line for line in data_generator())
     )
-    b = vertica_speed_test.test_insert_data(
+    a = ch_speed_test.test_insert_data_exec_time
+    vertica_speed_test.test_insert_data(
         data="test.csv",
         query="COPY test (id, viewpoint, date) FROM stdin DELIMITER ',' ",
     )
+    b = vertica_speed_test.test_insert_data_exec_time
+
     diff = b / a * 100 - 100
     print(f"Clickhouse быстрее записал данные на {round(diff, 1)}%")
 
 
 def test_read():
-    a = ch_speed_test.test_get_data("select * from test")
-    b = vertica_speed_test.test_get_data("select * from test")
+    ch_speed_test.test_get_data("select * from test")
+    a = ch_speed_test.test_get_data_exec_time
+    vertica_speed_test.test_get_data("select * from test")
+    b = vertica_speed_test.test_get_data_exec_time
     diff = b / a * 100 - 100
     print(f"Clickhouse быстрее считал данные на {round(diff, 1)}%")
 
