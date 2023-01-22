bc88bf5441091e283517e2b16926e12f1b0dc340
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-01-22 15:55:59 +0600

    Add crud

--- ugc_service/src/api/v1/review.py
+++ ugc_service/src/api/v1/review.py
@@ -7,17 +7,26 @@ from bson.objectid import ObjectId
 router = APIRouter()
 
 
-class Review(BaseModel):
+class ShortReview(BaseModel):
     text: str
     author: str
     publication_date: str
 
 
+class FullReview(BaseModel):
+    text: str
+    author: str
+    publication_date: str
+    movie_id: str
+
+
 @router.post("/{movie_id}")
-async def add_review(movie_id: str, review: Review,
-                     review_service: ReviewService = Depends(get_review_service),
-                     event_service: EventService = Depends(get_event_service)
-                     ):
+async def add_review(
+    movie_id: str,
+    review: ShortReview,
+    review_service: ReviewService = Depends(get_review_service),
+    event_service: EventService = Depends(get_event_service),
+):
     review_document = review.dict()
     review_document["movie_id"] = movie_id
 
@@ -28,11 +37,26 @@ async def add_review(movie_id: str, review: Review,
     return {"message": "Review added successfully."}
 
 
+@router.get("/{movie_id}")
+async def get_all_reviews(
+    movie_id: str, review_service: ReviewService = Depends(get_review_service)
+):
+    reviews = review_service.find({"movie_id": movie_id})
+
+    return [FullReview(**review) async for review in reviews]
+
+
 @router.put("/{review_id}")
-async def update_review(review_id: str, review: Review, review_service: ReviewService = Depends(get_review_service)):
+async def update_review(
+    review_id: str,
+    review: ShortReview,
+    review_service: ReviewService = Depends(get_review_service),
+):
     review_id = ObjectId(review_id)
 
-    result = await review_service.update_one({"_id": review_id}, {"$set": review.dict()})
+    result = await review_service.update_one(
+        {"_id": review_id}, {"$set": review.dict()}
+    )
     if result:
         return {"message": "Review updated successfully"}
     else:
@@ -40,7 +64,9 @@ async def update_review(review_id: str, review: Review, review_service: ReviewSe
 
 
 @router.delete("/{review_id}")
-async def delete_review(review_id: str, review_service: ReviewService = Depends(get_review_service)):
+async def delete_review(
+    review_id: str, review_service: ReviewService = Depends(get_review_service)
+):
     review_id = ObjectId(review_id)
 
     result = await review_service.delete_one({"_id": review_id})
