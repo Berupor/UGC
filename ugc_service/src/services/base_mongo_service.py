1c83997acd16166a7c9fb302dd0a13f9c832545d
Evgeniy Zelenov <zenek.zeka13@gmail.com>
2023-01-22 11:29:04 +0600

    Update service

--- ugc_service/src/services/base_mongo_service.py
+++ ugc_service/src/services/base_mongo_service.py
@@ -1,29 +1,72 @@
 from pymongo import MongoClient
+from core.config import settings
+from motor.motor_asyncio import AsyncIOMotorClient
 
 
 class BaseMongoService:
-
-    def __init__(self, db_name, collection_name):
-        self.client = MongoClient(
-            "mongodb+srv://eugene:iEW7kga69Y5qJucW@cluster0.t2rby2w.mongodb.net/?retryWrites=true&w=majority")
-
+    def __init__(
+        self,
+        db_name,
+        collection_name,
+        host=settings.mongo.host,
+        port=settings.mongo.port,
+        username=settings.mongo.username,
+        password=settings.mongo.password,
+    ):
+        # self.client = AsyncIOMotorClient("mongodb://root:rootpassword@localhost:27017/")
+        # self.db = self.client[db_name]
+        # self.collection = self.db[collection_name]
+        self.db_name = db_name
+        self.collection_name = collection_name
+        self.client = AsyncIOMotorClient(
+            f"mongodb://{username}:{password}@{host}:{port}/"
+        )
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
 
-    def create(self, document):
-        self.collection.insert_one(document)
-        print("Document created successfully.")
+    async def insert_one(self, document):
+        """Insert a single document into the collection"""
+        result = await self.collection.insert_one(document)
+        return result.inserted_id
 
-    def read(self, query={}):
-        return self.collection.find(query)
+    async def insert_many(self, documents):
+        """Insert multiple documents into the collection"""
+        result = await self.collection.insert_many(documents)
+        return result.inserted_ids
 
-    def update(self, query, new_values):
-        self.collection.update_many(query, {"$set": new_values})
-        print("Documents updated successfully.")
+    async def find_one(self, query):
+        """Find a single document that matches the query"""
+        document = await self.collection.find_one(query)
+        return document
 
-    def delete(self, query={}):
-        self.collection.delete_many(query)
-        print("Documents deleted successfully.")
+    async def find(self, query):
+        """Find all documents that match the query"""
+        cursor = self.collection.find(query)
+        documents = []
+        async for document in cursor:
+            documents.append(document)
+        return documents
 
-    def close(self):
-        self.client.close()
+    async def update_one(self, query, update):
+        """Update a single document that matches the query"""
+        result = await self.collection.update_one(query, update)
+        return result.modified_count
+
+    async def update_many(self, query, update):
+        """Update all documents that match the query"""
+        result = await self.collection.update_many(query, update)
+        return result.modified_count
+
+    async def delete_one(self, query):
+        """Delete a single document that matches the query"""
+        result = await self.collection.delete_one(query)
+        return result.deleted_count
+
+    async def delete_many(self, query):
+        """Delete all documents that match the query"""
+        result = await self.collection.delete_many(query)
+        return result.deleted_count
+
+    async def close(self):
+        """Close the MongoDB connection"""
+        await self.client.close()
--- ugc_service/src/services/review_service.py
+++ ugc_service/src/services/review_service.py
@@ -1,9 +1,13 @@
-from base_mongo_service import BaseMongoService
+from .base_mongo_service import BaseMongoService
 
 
 class ReviewService(BaseMongoService):
+
     pass
 
+    # def __init__(self, db_name, collection_name):
+    #     super.__init__(db_name, collection_name)
 
-def get_review_service(db_name, collection_name) -> BaseMongoService:
-    return BaseMongoService(db_name, collection_name)
+
+def get_review_service() -> ReviewService:
+    return ReviewService("movies", "reviews")
